574ddd062d22210bed00bbe3b5daa8eb7ff4d680
croixed <croixed@croixed>
2022-09-14 12:14:20 -0700

    added basic display of ratings

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,32 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import FeedbackList from './lib/FeedbackList.svelte'
+
+  let feedback = [
+    {
+      id: 1,
+      rating: 10,
+      text: 'nice',
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: 'good',
+    },
+    {
+      id: 3,
+      rating: 7,
+      text: 'mid',
+    }
+  ]
+ 
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <FeedbackList {feedback}/>
 
-  <div class="card">
-    <Counter />
-  </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
 </style>
